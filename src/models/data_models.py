986d3956b55b5fdb6a8d9e4c80639165393c2ca5
oladimeji balogun <oladimejib.tech@gmail.com>
2025-10-04 19:53:08 +0100

    implemented the data models with fiel validators

---
+++ src/models/data_models.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, Field, field_validator
+import re
+
+class DateTimeModel(BaseModel): 
+    date: str = Field(
+        description="properly formatted date", 
+        pattern=r'^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$'
+    )
+
+    @field_validator("date")
+    def check_date_format(cls, v): 
+        if not re.match(r'^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$', v): 
+            raise ValueError("The date should be in format 'DD-MM-YYYY HH:MM'")
+        return v
+    
+
+
+class DateModel(BaseModel): 
+    date: str = Field(
+        description="properly formatted date", 
+        pattern=r'^\d{2}-\d{2}-\d{4}$'
+    )
+
+    @field_validator("date")
+    def check_date_format(cls, v): 
+        if not re.match(r'^\d{2}-\d{2}-\d{4}$', v): 
+            raise ValueError("the date must be in this format 'DD-MM-YYY'")
+        return v
+    
+class IDNumberModel(BaseModel): 
+    id: int = Field(
+        description="identification number (7 or 8 digits long)", 
+    )
+
+    @field_validator("id")
+    def check_id_format(cls, v): 
+        if not re.match(r'^\d{7, 8}$', str(v)):
+            raise ValueError("the ID number should be a 7 or 8 digit number")
+        return v
